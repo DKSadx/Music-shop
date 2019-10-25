50209e89a4796a7352c411716c4489b3fa0bdd2a
DKSadx <sadx.carmon@gmail.com>
2019-10-25 18:59:08 +0000

    Fix for react-router when the root isn't "/"

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+
 import App from './App';
+import './index.scss';
+import { baseName } from './utils/consts';
 
 const app = (
-  <BrowserRouter>
+  <BrowserRouter basename={baseName}>
     <App />
   </BrowserRouter>
 );
