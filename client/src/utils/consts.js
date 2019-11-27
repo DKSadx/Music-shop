687a5baeb1e402d06882da2396e7f3c0c244d0f9
DKSadx <sadx.carmon@gmail.com>
2019-11-27 22:43:01 +0000

    Production routes added

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,6 +1,6 @@
 // Email validation regex
 // eslint-disable-next-line
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const baseName = ''; // If the app is in subfolder set it here ('/fooSubfolder')
-// export const baseIp = 'https://api.darkokojovic.com'; // Api ip address
-export const baseIp = 'http://localhost:8080'; // Api ip address
+export const baseName = "/music-shop"; // If the app is in subfolder set it here ('/fooSubfolder')
+export const baseIp = "https://api.darkokojovic.com"; // Api ip address
+// export const baseIp = 'http://localhost:8080'; // Api ip address
