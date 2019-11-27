8481c6cf9c19ada93ff25e136fad28b2468a218e
DKSadx <sadx.carmon@gmail.com>
2019-11-27 22:46:24 +0000

    CI/CD test

--- client/src/components/Slideshow/text.js
+++ client/src/components/Slideshow/text.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React from "react";
 export const slideshowText = [
-  <div className="slide-text">Slide1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint adipisci hic, culpa dignis</div>,
-  <div className="slide-text">Slide2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint adipisci hic, culpa dignis</div>,
-  <div className="slide-text">Slide3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint adipisci hic, culpa dignis</div>
+  <div className="slide-text">
+    Slide1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint
+    adipisci hic, culpa dignis dignis
+  </div>,
+  <div className="slide-text">
+    Slide2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint
+    adipisci hic, culpa dignis
+  </div>,
+  <div className="slide-text">
+    Slide3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sint
+    adipisci hic, culpa dignis
+  </div>
 ];
