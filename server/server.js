81551e2c8efbc6de855798fa430dac865025e09d
DKSadx <sadx.carmon@gmail.com>
2019-12-15 10:02:51 +0000

    CI/CD backend test

--- server/server.js
+++ server/server.js
@@ -34,12 +34,17 @@ const fileFilter = (req, file, cb) => {
   }
 };
 app.use(bodyParser.json());
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
+);
 app.use('/images', express.static(path.join(__dirname, '/images')));
 // Headers for allowing request, client<->server
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
@@ -48,6 +53,8 @@ app.use((req, res, next) => {
 // const os = require('os');
 // const networkInterfaces = os.networkInterfaces();
 // console.log(networkInterfaces.wlo1[0].address);
+
+app.get('/test', (req, res) => res.send(':)'));
 
 // Routes
 app.use('/category', categoryRoutes);
